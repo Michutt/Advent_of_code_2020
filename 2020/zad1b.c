13b754e7a3242bbc930bfe4c86ac7eec2dee2564
Michutt <niemc.michal@gmail.com>
2021-02-06 19:10:36 +0100

    add zad1b.c

---
+++ 2020/zad1b.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *file_parse()
+{
+   int i = 1, lines = 0;
+   char ch[5];
+   FILE *myfile;
+   myfile = fopen("input.txt","r");
+
+   while (!feof(myfile))
+      if (fgetc(myfile) == '\n') lines++;
+   lines++;
+   rewind(myfile);
+
+   int* data = malloc(sizeof(int)*lines);
+   data[0] = lines;
+
+   while(lines--){
+      fscanf(myfile,"%s",ch);
+      data[i] = atoi(ch);
+      i++;
+   }
+
+   fclose(myfile);
+   return data;
+}
+
+int find_entries(int *data)
+{
+   int lines = data[0];
+   data++;
+   for (int i = 0; i < lines; i++)
+      for (int j = 0; j < lines; j++)
+         for (int k = 0; k < lines; k++)
+            if (data[i] + data[j] + data[k] == 2020)
+               printf("%d+%d+%d=2020   ans=%d\n", data[i], data[j], data[k], data[i]*data[j]*data[k]);
+}
+
+int main() {
+   int *data = file_parse();
+   find_entries(data);
+   getchar();
+   return 0;
+}
